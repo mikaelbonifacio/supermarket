7058571fbc2d98ad42d54ee3e1a89ae80f36a3a2
Bonifacio <mikaelbonifacio@gmail.com>
2024-06-16 17:00:48 -0300

    Create Switch menu inside do while loop

--- src/main/java/implementation/Main.java
+++ src/main/java/implementation/Main.java
@@ -1,8 +1,45 @@
 package main.java.implementation;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private final static int SIZE = 3;
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Supermarket supermarket = new SupermarketArray(SIZE);
+        int opcao;
+        do {
+            System.out.println("\nLista de Compras");
+            System.out.println("1 - Inserir");
+            System.out.println("2 - Listar");
+            System.out.println("3 - Remover");
+            System.out.println("4 - Sair");
+            System.out.println("Escolha uma opção: ");
+            opcao = scanner.nextInt();
 
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o item ser inserido: ");
+                    String item = scanner.next(); // apenas palavras únicas.
+                    supermarket.add(item);
+                    break;
+                case 2:
+                    supermarket.print();
+                    break;
+                case 3:
+                    System.out.println("Digite a posição do Item a ser removido: ");
+                    int index = scanner.nextInt();
+                    supermarket.delete(index);
+                    break;
+                case 4:
+                    System.out.println("Saindo do programa...");
+                default:
+                    System.out.println("Opção inválida, por favor, escolha novamente.");
+            }
 
+        } while (opcao != 4);
+
+        scanner.close();
     }
 }
